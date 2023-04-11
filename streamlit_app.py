c9a3a0aae3f7ff7f8f2d3316626746c97020bdcc
sglynnsf2 <130496416+sglynnsf2@users.noreply.github.com>
2023-04-11 14:41:05 -0400

    Update streamlit_app.py
    pre-populate options

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #display table
 streamlit.dataframe(my_fruit_list)
